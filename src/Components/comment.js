c013e83d87b54a6a93e4f697651a679ac5b6a1ab
Hao <hao.qiao7@gmail.com>
2018-07-29 21:53:36 -0400

    final commit

--- src/Components/comment.js
+++ src/Components/comment.js
@@ -5,8 +5,17 @@ class Comment extends Component{
     render(){
         return (
             <div className='comment'>
-                <div style={{width: '20%', height: '100%', padding: '6%', borderRight: '1px #999 solid'}}>
+                <div style={{width: '20%', height: '100%', padding: '6%', borderRight: '1px #999 solid', float: 'left'}}>
                     <FontAwesomeIcon icon="th" size='lg'/>
+                </div>
+                <div style={{width: '20%', float: 'right', height: '100%', marginRight: '10%'}}>
+                    <label className="switch" style={{marginTop: '18%'}}>
+                        <input type="checkbox"/>
+                        <span className="slider round">
+                            <span style={{fontSize: '0.8em'}}>&nbsp;&nbsp;Yes&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                            <span style={{fontSize: '0.8em'}}>No</span>
+                        </span>
+                    </label>
                 </div>
             </div>
         );
